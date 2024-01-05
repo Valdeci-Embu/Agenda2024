79d60702ff2248ea6978a8a9c03d571cd59fbd04
Valdeci <valdecie@gmail.com>
2024-01-04 21:28:49 -0300

    first commit

---
+++ contact/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from contact import models
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'category',)
+    ordering = ('first_name', 'id',)
+    search_fields = ("id", "first_name", "last_name", 'category')
+    list_filter = ('category', 'created_date')
+    list_per_page = 8
+    list_max_show_all = 100
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', )
+    ordering = ('name', 'id',)
