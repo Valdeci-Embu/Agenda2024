5f5779449049b10adafe2c1b30baff8220bbb7aa
Valdeci <valdecie@gmail.com>
2024-01-05 21:34:58 -0300

    paginação

--- contact/admin.py
+++ contact/admin.py
@@ -3,12 +3,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone', 'category',)
+    list_display = ('id', 'first_name', 'last_name', 'phone', 'category', 'show',)
     ordering = ('first_name', 'id',)
     search_fields = ("id", "first_name", "last_name", 'category')
     list_filter = ('category', 'created_date')
     list_per_page = 8
     list_max_show_all = 100
+    list_editable = ('show',)
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
